9dcad76e085aca00690e3c7c5c5151e93474948a
LukeJohnson <lukejohnsonmail@gmail.com>
2014-09-13 12:44:06 -0400

    Update GreetingController.java

--- src/main/java/hello/controller/GreetingController.java
+++ src/main/java/hello/controller/GreetingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello person named %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
